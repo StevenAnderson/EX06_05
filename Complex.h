a52802fc0cbce11faf967b582477950851dec6fd
Steven Anderson <Steven@Mr-Anderson-2.local>
2015-06-12 23:00:29 -0700

    final

---
+++ Complex.h
@@ -0,0 +1,64 @@
+#ifndef __EX06_05__Complex__
+#define __EX06_05__Complex__
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+//creat class
+class Complex{
+private:
+    //two private variables, real and imaginary
+    float a;
+    float b;
+    
+    
+public:
+    //Constructors
+    Complex (float a,float b){
+        this->a=a;
+        this->b=b;
+    };
+    Complex(float a){
+        this->a=a;
+        b=0;
+    };
+    Complex(){
+        a=0;
+        b=0;
+    };
+    
+//base functions for math
+Complex add(Complex& c);
+Complex subtract(Complex& c);
+Complex multiply(Complex& c);
+Complex divide(Complex& c);
+float abs();
+
+string toString();
+
+float getRealPart();
+float getImaginaryPart();
+    
+    //nonmember functions?
+Complex operator+(Complex c);
+Complex operator-(Complex c);
+Complex operator*(Complex c);
+Complex operator/(Complex c);
+
+
+//overloading operatrs
+    float operator[](int i);
+    void operator=(Complex c);
+    string operator<<(Complex c);
+    string operator>>(Complex c);
+    void operator+=(Complex c);
+    void operator-=(Complex c);
+    Complex operator/=(Complex c);
+    Complex operator*=(Complex c);
+    void operator--();
+    void operator++();
+
+
+};
+
+#endif
